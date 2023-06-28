6e59a0d89562665ea558a052d884788595632915
Phạm Phúc Lợi <105093870+phamphucloi@users.noreply.github.com>
2023-06-28 13:21:49 -0700

    213

--- src/app/Flight/flight.component.ts
+++ src/app/Flight/flight.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from "@angular/core";
+import { CarService } from "../Services/carService";
+import { Car } from "../Models/car.model";
 
 @Component({
     templateUrl: './flight.component.html'
 })
 
 export class FlightComponent implements OnInit {
-    ngOnInit(): void {
 
+    constructor(private _carService:CarService){}
+
+    cars: Car[];
+
+    ngOnInit(): void {
+        this._carService.getAllCar().then(res=>{
+            this.cars = res as Car[];
+        },er=>console.log(er));
+    }
+
+    chooseOption(a: any){
+        console.log(a.target.value);
+        
     }
     
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FlightComponent } from './Flight/flight.component';
+
 import { HomeBusComponent } from './Home_Bus/homeBus.component';
 import { CartComponent } from './Cart/cart.component';
 import { AddBusComponent } from './Add_Bus/add.component';
@@ -15,6 +15,7 @@ import { CategoryCarService } from './Services/categoryCarService';
 import { CarService } from './Services/carService';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlightComponent } from './Flight/flight.component';
 
 @NgModule({
   declarations: [
